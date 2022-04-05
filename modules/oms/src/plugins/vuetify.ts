64033f7c16e199cc0668713f4dac461ff27acb5e
sammandanu <33632361+sammandanu@users.noreply.github.com>
2022-04-05 23:37:56 +0700

    chore(oms): update color palette (#52)

--- modules/oms/src/plugins/vuetify.ts
+++ modules/oms/src/plugins/vuetify.ts
@@ -1,20 +1,21 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 const theme = {
-    primary: '#3856F6',
-    secondary: '#E91E63',
-    accent: '#3856F6',
-    info: '#00CAE3',
-  }
+  primary: "#3856F6",
+  secondary: "#2D3748",
+  accent: "#3856F6",
+  info: "#00CAE3",
+  error: "#FF0000",
+};
 
 export default new Vuetify({
-    theme: {
-        themes: {
-          dark: theme,
-          light: theme,
-        },
-      },
+  theme: {
+    themes: {
+      dark: theme,
+      light: theme,
+    },
+  },
 });
