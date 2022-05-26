1a2387a5134580730c9f6b5c6757b3284775dab3
sammandanu <33632361+sammandanu@users.noreply.github.com>
2022-05-27 03:36:24 +0700

    chore(pbf): redesign card information on confirmation page (#119)

--- modules/patient-booking-frontend/src/views/ReviewAppointment.vue
+++ modules/patient-booking-frontend/src/views/ReviewAppointment.vue
@@ -6,102 +6,103 @@
       </div>
       <div class="pt-7">
         <h1 class="text-xl">Review Appointment</h1>
-        <p class="pt-3 text-base lg:text-lg font-normal text-gray-700 antialiased">
-          Please review your order
+        <p
+          class="pt-3 text-base lg:text-lg font-normal text-gray-700 antialiased"
+        >
+          Please review your information
         </p>
       </div>
-      <div class="pt-10 pb-7 grid md:grid md:grid-cols-2 grid-cols-1 gap-y-7 gap-x-10 md:gap-x-10">
-        <div class="grid grid-cols-1 gap-y-7">
-          <div class="gap-y-7">
-            <div class="grid grid-cols-2">
-              <label for="firstname" class="font-extralight text-sm">First Name</label>
-              <label for="lastname" class="font-extralight text-sm">Last Name</label>
-            </div>
-            <div class="grid grid-cols-2">
-              <p>{{ $store.getters.firstName }}</p>
-              <p>{{ $store.getters.lastName }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
-          <div class="gap-y-7">
-            <div>
-              <label for="phonenumber" class="font-extralight text-sm">Phone</label>
-            </div>
-            <div>
-              <p>{{ $store.getters.phoneNumber }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
-          <div class="gap-y-7">
-            <div class="grid grid-cols-2">
-              <label for="gender" class="font-extralight text-sm">Gender</label>
-              <label for="dob" class="font-extralight text-sm">Date of Birth</label>
-            </div>
-            <div class="grid grid-cols-2">
-              <p>
-                {{ $store.getters.gender }}
-              </p>
-              <p>{{ printDate($store.getters.dob) }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
-          <div class="gap-y-7">
-            <div>
-              <label for="address" class="font-extralight text-sm">Address</label>
-            </div>
-            <div>
-              <p>{{ $store.getters.locationAddress }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
-          <div class="gap-y-7">
-            <div>
-              <label for="apartment" class="font-extralight text-sm">Apartment #</label>
-            </div>
-            <div class="grid grid-cols-2">
-              <p>{{ $store.getters.locationApartment }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
-          <div class="gap-y-7">
-            <div class="grid grid-cols-2">
-              <label for="gender" class="font-extralight text-sm">Date</label>
-              <label for="dob" class="font-extralight text-sm">Time</label>
-            </div>
-            <div class="grid grid-cols-2">
-              <p>
-                {{ printDate($store.getters.date) }}
-              </p>
-              <p>{{ formatTimeSlot($store.getters.startTime) }} - {{ formatTimeSlot($store.getters.endTime) }}</p>
-            </div>
-            <hr class="w-80"/>
-          </div>
+      <div class="my-6 px-4 pb-6 pt-3 rounded-xl border border-darkGray">
+        <div
+          class="h-8 w-8 rounded-full flex items-center col-span-2 mb-3 -pl-1 justify-center border flex-none border-darkGray"
+        >
+          <wz-icon name="info" color="darkGray" />
         </div>
-        <div>
-          <div class=" rounded p-4 cursor-pointer border flex justify-center">
-            <div class="h-8 w-8 rounded-full flex items-center justify-center border flex-none border-darkGray border-stroke">
-              <wz-icon name="info" color="darkGray" />
+
+        <div class="grid grid-cols-2 gap-4">
+          <div class="font-semibold">Patient Information</div>
+          <div class="grid grid-cols-12">
+            <div class="">:</div>
+            <div class="col-span-11">
+              <div class="">
+                {{ $store.getters.firstName }} {{ $store.getters.lastName }}
+              </div>
+
+              <div class="">{{ printDate($store.getters.dob) }}</div>
+              <div class="">{{ $store.getters.gender }}</div>
+              <div class="">{{ $store.getters.phoneNumber }}</div>
+              <div class="">{{ $store.getters.email }}</div>
             </div>
-            <div class="py-5 flex-1 mx-4">
-              <div class="mx-4 flex flex-col items-center justify-center">
-                <!-- TODO: console error coming from image. Passing from api will resolve this issue -->
-                <img :src="$store.getters.serviceTypeImage" class="mb-4 h-12" :alt="image" />
-                <p class="text-fontPrimary mb-2">{{ $store.getters.serviceTypeName }}</p>
-                <p class="font-normal text-fontSecondary mb-2">{{$store.getters.serviceTypeDescription }}</p>
-                <p class="font-normal text-primary"> {{ isInsurance ? '' : '$ ' + $store.getters.serviceTypePrice }}</p>
+          </div>
+          <div class="font-semibold">Address</div>
+          <div class="grid grid-cols-12">
+            <div class="">:</div>
+            <div class="col-span-11">
+              <div class="">
+                {{ $store.getters.locationAddress
+                }}{{
+                  $store.getters.locationApartment
+                    ? ', ' + $store.getters.locationApartment
+                    : ''
+                }}
               </div>
             </div>
           </div>
-          <div class="pt-5 flex justify-between">
-            <p class="text-gray-500"> {{ isInsurance ? '' : 'Total: ' }} </p>
-            <p class="font-normal text-primary"> {{ isInsurance ? '' : '$ ' + $store.getters.serviceTypePrice }}</p>
+          <div class="font-semibold">Appointment Date</div>
+          <div class="grid grid-cols-12">
+            <div class="">:</div>
+            <div class="col-span-11">
+              <div class="">
+                {{ formatDate($store.getters.date) }}
+              </div>
+              <div class="">
+                {{ formatTimeSlot($store.getters.startTime) }} -
+                {{ formatTimeSlot($store.getters.endTime) }}
+              </div>
+            </div>
+          </div>
+          <div class="font-semibold">Reason for Appointment</div>
+          <div class="grid grid-cols-12">
+            <div class="">:</div>
+            <div class="col-span-11">
+              <div class="">{{ $store.getters.serviceTypeName }}</div>
+              <div class="" v-if="$store.getters.notes">
+                {{ $store.getters.notes }}
+              </div>
+            </div>
+          </div>
+          <div class="font-semibold" v-if="isInsurance">Total</div>
+          <div class="grid grid-cols-12" v-if="isInsurance">
+            <div class="">:</div>
+            <div class="col-span-11">
+              <div class="">${{ $store.getters.serviceTypePrice }}</div>
+            </div>
           </div>
         </div>
       </div>
+
       <div class="pt-5">
         <wz-button color="primary" @click="createAppointment" block>
           <p class="text-white">Confirm Appointment</p>
         </wz-button>
+      </div>
+      <div class="pt-5">
+        <p class="text-gray-400 text-center text-sm">
+          Additionally, you agree to our
+          <a
+            href="https://www.welz.com/privacy"
+            class="text-primary"
+            target="_blank"
+            >Privacy Policy</a
+          >
+          and
+          <a
+            href="https://www.welz.com/terms"
+            class="text-primary"
+            target="_blank"
+            >Terms of Service</a
+          >.
+        </p>
       </div>
       <div class="pt-4 items-center">
         <wz-button type="button" block text @click="$router.back()">
@@ -110,17 +111,18 @@
       </div>
     </div>
     <wz-snackbars v-model="snackbar.open" color="fontPrimary" :timeout="6000">
-        <template>
-          <div class="w-80 pl-4 text-white">{{ snackbar.message }}</div>
-        </template>
-        <template #action>
-          <wz-button
-            text
-            @click="snackbar.open= false"
-            color="red"
-            class="text-red mr-4"
-            >Close</wz-button>
-        </template>
+      <template>
+        <div class="w-80 pl-4 text-white">{{ snackbar.message }}</div>
+      </template>
+      <template #action>
+        <wz-button
+          text
+          @click="snackbar.open = false"
+          color="red"
+          class="text-red mr-4"
+          >Close</wz-button
+        >
+      </template>
     </wz-snackbars>
   </div>
 </template>
@@ -179,14 +181,17 @@ export default Vue.extend({
       }
       console.log(appointment)
     },
+    formatDate (isoDate: string) {
+      return moment(isoDate).format('dddd, MMMM Do YYYY')
+    },
     formatTimeSlot (time: string) {
       let hour = parseInt(time.slice(0, 2))
       const suffix = hour < 12 ? ' AM' : ' PM'
       hour = hour > 12 ? hour - 12 : hour
-      return ((hour < 10 ? '0' : '') + hour + suffix)
+      return (hour < 10 ? '0' : '') + hour + suffix
     },
     printDate (date: string): string {
-      return moment(date).format('MMMM DD YYYY')
+      return moment(date).format('MM/DD/YYYY')
     }
   },
   computed: {
